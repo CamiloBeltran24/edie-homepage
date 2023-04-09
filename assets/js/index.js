23e06eb2c071c6cb48130cbb92a6c1139b9de26e
Camilo Beltran <cambelf.dev@gmail.com>
2023-04-09 16:49:57 -0500

    Update Mobile

---
+++ assets/js/index.js
@@ -0,0 +1,14 @@
+(() => {
+  'use strict';
+
+  let burgerMenu = document.querySelector('.js-burger-menu');
+
+  burgerMenu.addEventListener('click', () => {
+    showNavigation();
+  })
+
+  function showNavigation(){
+    const navigation = document.querySelector('.navigation');
+    navigation.classList.toggle('active');
+  }
+})();
